2c67802f1bcc595af39ebe15942e7655cf3af5ae
jessyguna98 <38969746+jessyguna98@users.noreply.github.com>
2019-06-06 09:49:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import json
 from flask import Flask, request, make_response, jsonify
+app = Flask(__name__)
+log = app.logger
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    """This method handles the http requests for the Dialogflow webhook
+    This is meant to be used in conjunction with the weather Dialogflow agent
+    """
+    req = request.get_json(silent=True, force=True)
+    try:
+        action = req.get('queryResult').get('action')
+    except AttributeError:
+        return 'json error'
+
+    #res = "Hurray!"
+
+    if action == 'migration':
+res = is_valid_doctor(req)
